f8684df33e0b4a64de4c109e1b674f33b416bd2d
Parash Chand <71168469+parashchand10@users.noreply.github.com>
2026-06-20 10:01:52 +0530

    Update titanic.py

--- titanic.py
+++ titanic.py
@@ -25,7 +25,7 @@ st.markdown("""
     }
     #MainMenu, footer, header {visibility: hidden;}
     .block-container {
-        padding-top: 1.5rem !important;
+        padding-top: 0.5rem !important;
     }
 
     /* App title / hero */
